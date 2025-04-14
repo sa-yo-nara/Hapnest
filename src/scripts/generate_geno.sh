50bc8109222798453e29a5c08c280293b4247c12
Ubuntu <ubuntu@ip-172-31-22-37.eu-south-1.compute.internal>
2025-04-14 09:44:54 +0000

    removed old packages

--- src/scripts/generate_geno.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -x
-# Generate synthetic genotypes
-CURR_DIR=`pwd`
-SCRIPT_DIR=$(dirname "$0")
-cd $SCRIPT_DIR
-julia --threads "$1" ../run_program.jl --genotype --config "$2"
-cd $CURR_DIR
